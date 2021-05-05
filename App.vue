5d1ddefccae52784d6e1e25761630491586c7372
Leo23-23 <58565201+Leo23-23@users.noreply.github.com>
2021-05-05 02:12:36 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,27 @@
+  
+<template>
+  <div id="app">
+    <!--<Pokemon />-->
+    <PremierePage />
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+import Pokemon from './components/Pokemon.vue'
+import PremierePage from './components/PremierePage.vue'
+export default {
+  components: {
+    Pokemon,
+    PremierePage
+  }
+}
+</script>
+
+<style lang="scss">
+  @import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+  body {
+    margin: 0;
+    padding: 0;
+  }
+</style>
